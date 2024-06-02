f8b33bf35036843381206468b895671836ac3382
aGuegu <weihong.guan@gmail.com>
2024-06-02 18:24:23 +0800

    aht21 included

--- index.ts
+++ index.ts
@@ -10,42 +10,45 @@ namespace Atiic {
   }
 
   const addressBH1750 = 0x23;
+  const addressAht21 = 0x38;
 
-  //% blockId="bh1750Init" block="bh1750.init"
+  //% blockId="bh1750Init" block="BH1750.init"
   //% color=#a96836
   export function bh1750Init(): void {
     pins.i2cWriteBuffer(addressBH1750, array2buffer([0x10]));
     pins.i2cWriteBuffer(addressBH1750, array2buffer([0x42, 0x65]));
   }
 
-  //% blockId="bh1750Intensity" block="bh1750.measure"
-  //% weight=80 blockGap=8
+  //% blockId="bh1750Intensity" block="BH1750.intensity"
+  //% color=#a96836
   export function bh1750Intensity(): number {
     basic.pause(120);
     return Math.idiv(pins.i2cReadNumber(addressBH1750, NumberFormat.UInt16BE) * 5, 6);
   }
 
-  // export function init(): void {
-  //   const status = pins.i2cReadNumber(Address, NumberFormat.UInt8BE);
-  //   if (!(status & 0x08)) {
-  //     pins.i2cWriteBuffer(Address, array2buffer([0xbe, 0x08, 0x00]));
-  //     basic.pause(10);
-  //   }
-  // }
-  //
-  // //% blockId="AHT21_humidity" block="aht21 humidity"
-  // //% weight=80 blockGap=8
-  // export function humidiy(): number {
-  //   pins.i2cWriteBuffer(Address, array2buffer([0xac, 0x33, 0x08]));
-  //   basic.pause(80);
-  //
-  //   const buff = pins.i2cReadBuffer(Address, 7);
-  //
-  //   const humidiy = buff.getNumber(NumberFormat.UInt32BE, 1);
-  //
-  //   humidiy >>= 12;
-  //   humidiy /= 1 << 20;
-  //
-  //   return humidiy;
-  // }
+  //% blockId="aht21Init" block="AHT21.init"
+  //% color=#3677a9
+  export function aht21Init(): void {
+    const status = pins.i2cReadNumber(Address, NumberFormat.UInt8BE);
+    if (!(status & 0x08)) {
+      pins.i2cWriteBuffer(Address, array2buffer([0xbe, 0x08, 0x00]));
+      basic.pause(10);
+    }
+  }
+
+  //% blockId="aht21Humidity" block="AHT21.humidity"
+  //% color=#3677a9
+  export function aht21Humidiy(): number {
+    pins.i2cWriteBuffer(Address, array2buffer([0xac, 0x33, 0x08]));
+    basic.pause(80);
+
+    const buff = pins.i2cReadBuffer(Address, 7);
+
+    const humidiy = buff.getNumber(NumberFormat.UInt32BE, 1);
+
+    humidiy >>= 12;
+    humidiy /= 1 << 20;
+    
+    return humidiy;
+  }
 }
