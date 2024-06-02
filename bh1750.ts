37fbe22f3528109bcf62fdab638549446358cf52
aGuegu <weihong.guan@gmail.com>
2024-06-02 14:06:56 +0800

    measureTime configured

--- bh1750.ts
+++ bh1750.ts
@@ -6,30 +6,32 @@ namespace BH1750 {
 
   const Address = 35;
 
-    /**
-     * turn on BH1750.
-     */
-    //% blockId="BH1750_ON" block="turn on"
-    //% weight=90 blockGap=8
-    export function on(): void {
-        pins.i2cWriteNumber(Address, 0x10, NumberFormat.UInt8BE)
-    }
+  /**
+   * turn on BH1750.
+   */
+  //% blockId="BH1750_ON" block="turn on"
+  //% weight=90 blockGap=8
+  export function on(): void {
+    pins.i2cWriteNumber(Address, 0x10, NumberFormat.UInt8BE);
+    pins.i2cWriteNumber(Address, 0x4265, NumberFormat.UInt16BE);
+  }
 
     /**
      * turn off BH1750, to reduce power consumption.
      */
     //% blockId="BH1750_OFF" block="turn off"
     //% weight=90 blockGap=8
-    export function off(): void {
-        pins.i2cWriteNumber(Address, 0, NumberFormat.UInt8BE)
-    }
+  export function off(): void {
+    pins.i2cWriteNumber(Address, 0, NumberFormat.UInt8BE);
+  }
 
-    /**
-     * get ambient light data (lx)
-     */
-    //% blockId="BH1750_GET_INTENSITY" block="get intensity (lx)"
-    //% weight=80 blockGap=8
-    export function getIntensity(): number {
-        return Math.idiv(pins.i2cReadNumber(Address, NumberFormat.UInt16BE) * 5, 6)
-    }
+  /**
+   * get ambient light data (lx)
+   */
+  //% blockId="BH1750_GET_INTENSITY" block="get intensity (lx)"
+  //% weight=80 blockGap=8
+  export function getIntensity(): number {
+    basic.pause(120);
+    return Math.idiv(pins.i2cReadNumber(Address, NumberFormat.UInt16BE) * 5, 6);
+  }
 }
