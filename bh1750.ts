380dbb67bb54e674093650ff107c3ff56d2cac6d
aGuegu <weihong.guan@gmail.com>
2024-06-02 17:23:16 +0800

    try array2buffer

--- bh1750.ts
+++ bh1750.ts
@@ -1,34 +1,34 @@
-
-
-
 //% weight=100 color=#a96836 icon="\uf185"
 namespace BH1750 {
+  const Address = 0x23;
 
-  const Address = 35;
+  function array2buffer (list: number[]) {
+    const buff = pins.createBuffer(list.length);
+    list.forEach((x, i) => {
+      buff.setNumber(NumberFormat.UInt8LE, i, x);
+    });
+    return buff;
+  }
 
-  /**
-   * turn on BH1750.
-   */
-  //% blockId="BH1750_init" block="bh1750 init"
+  //% blockId="BH1750_init" block="bh1750.init"
   //% weight=90 blockGap=8
   export function init(): void {
-    pins.i2cWriteNumber(Address, 0x10, NumberFormat.UInt8BE);
-    pins.i2cWriteNumber(Address, 0x4265, NumberFormat.UInt16BE);
+    // pins.i2cWriteNumber(Address, 0x10, NumberFormat.UInt8BE);
+    pins.i2cWriteBuffer(Address, array2buffer([0x10]));
+    // let buff = pins.createBuffer(2);
+    // buff.setNumber(NumberFormat.UInt8LE, 0, 0x42);
+    // buff.setNumber(NumberFormat.UInt8LE, 1, 0x65);
+    pins.i2cWriteBuffer(Address, array2buffer([0x42, 0x65]));
+    // pins.i2cWriteNumber(Address, 0x4265, NumberFormat.UInt16BE);
   }
 
-    /**
-     * turn off BH1750, to reduce power consumption.
-     */
-    //% blockId="BH1750_deinit" block="bh1750 deinit"
-    //% weight=90 blockGap=8
-  export function deinit(): void {
-    pins.i2cWriteNumber(Address, 0, NumberFormat.UInt8BE);
-  }
+  // //% blockId="BH1750_deinit" block="bh1750.deinit"
+  // //% weight=90 blockGap=8
+  // export function deinit(): void {
+  //   pins.i2cWriteNumber(Address, 0, NumberFormat.UInt8BE);
+  // }
 
-  /**
-   * get ambient light data (lx)
-   */
-  //% blockId="BH1750_measure" block="bh1750 measure"
+  //% blockId="BH1750_measure" block="bh1750.measure"
   //% weight=80 blockGap=8
   export function measure(): number {
     basic.pause(120);
